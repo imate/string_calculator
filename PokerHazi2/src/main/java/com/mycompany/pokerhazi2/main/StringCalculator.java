b3bf87def0258cf6d2492eb7ab22196eb70d035c
imate <imateee93@gmail.com>
2015-02-20 21:01:00 +0100

    üres stringet, vagy egy számot elfogad

--- PokerHazi2/src/main/java/com/mycompany/pokerhazi2/main/StringCalculator.java
+++ PokerHazi2/src/main/java/com/mycompany/pokerhazi2/main/StringCalculator.java
@@ -5,5 +5,11 @@ package com.mycompany.pokerhazi2.main;
  * @author imate
  */
 public class StringCalculator {
-    
+    public static int add(String numbers){
+        int sum=0;
+        if(!numbers.equals("")){
+            sum+=Integer.parseInt(numbers);
+        }
+        return sum;
+    }
 }
